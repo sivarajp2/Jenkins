603b87a05355d1d60bf6a268d658fecb609ce3db
sivarajp2 <sivaraj@live.com>
2017-12-01 19:31:04 -0600

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -6,7 +6,7 @@ region = "us-east-1"
 resource "aws_instance" "example"{
 ami = "ami-55ef662f",
 instance_type = "t2.micro"
-key_name = "siva"
+key_name = "sivarajnew"
 tags {
         Name = "TestEC2"
     }
