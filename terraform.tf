9d2a089c45cf3c7280a01ae0328fd6f5490efea6
sivarajp2 <sivaraj@live.com>
2018-01-30 07:18:49 -0600

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-access_key = "AKIAJDHWPZTWHZPCY7XQ"
-secret_key = "yzIvadf6wpdj/v7gJxYuHWH2vBbIkj9WzWuDMOoF"
+access_key = "AXQ"
+secret_key = "oF"
 region = "us-east-1"
 }
 resource "aws_instance" "example"{
